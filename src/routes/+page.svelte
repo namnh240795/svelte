e26c30574061cdea230b6b47cd88749f867ea34d
namnh240795 <namnh240795@gmail.com>
2023-08-15 22:41:54 +0700

    Add svelte head

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,16 @@
 	import Layout from '../components/Layout.svelte';
 </script>
 
-<title>Nguyen Hoai Nam</title>
-<meta property="title" content="Nguyen Hoai Nam" />
-<meta property="url" content="https://cv.yellowcandle.party" />
-<meta name="description" content="Senior Developer" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="Nguyen Hoai Nam" />
-<meta property="og:description" content="Senior Developer" />
-<meta name="keywords" content="Nguyen Hoai Nam" />
-<meta name="author" content="Nguyen Hoai Nam" />
-<meta property="og:image" content="https://cv.yellowcandle.party/avatar.jpeg" />
+<svelte:head>
+	<title>Nguyen Hoai Nam</title>
+	<meta property="title" content="Nguyen Hoai Nam" />
+	<meta property="url" content="https://cv.yellowcandle.party" />
+	<meta name="description" content="Senior Developer" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Nguyen Hoai Nam" />
+	<meta property="og:description" content="Senior Developer" />
+	<meta name="keywords" content="Nguyen Hoai Nam" />
+	<meta name="author" content="Nguyen Hoai Nam" />
+	<meta property="og:image" content="https://cv.yellowcandle.party/avatar.jpeg" />
+</svelte:head>
 <Layout />
