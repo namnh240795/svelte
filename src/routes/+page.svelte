dab9598864544fa8c2dbb5c7300887cd51d8a8e2
namnh240795 <namnh240795@gmail.com>
2023-08-15 22:38:41 +0700

    Add SEO tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
-    import Avatar from '../components/Avatar.svelte';
-	import Contact from '../components/Contact.svelte';
 	import Layout from '../components/Layout.svelte';
-    import Text from '../components/Text.svelte'
 </script>
 
+<title>Nguyen Hoai Nam</title>
+<meta property="title" content="Nguyen Hoai Nam" />
+<meta property="url" content="https://cv.yellowcandle.party" />
+<meta name="description" content="Senior Developer" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Nguyen Hoai Nam" />
+<meta property="og:description" content="Senior Developer" />
+<meta name="keywords" content="Nguyen Hoai Nam" />
+<meta name="author" content="Nguyen Hoai Nam" />
+<meta property="og:image" content="https://cv.yellowcandle.party/avatar.jpeg" />
 <Layout />
