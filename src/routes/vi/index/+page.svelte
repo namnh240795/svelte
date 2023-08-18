bd3eb2487b15b8ce8ff7628a8c7829fe9eed4b22
Nam Nguyen <namnh240795@gmail.com>
2023-08-18 15:09:35 +0700

    fix: modify folder structure to support index.html

---
+++ src/routes/vi/index/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { setContext } from 'svelte';
+	import Layout from '../../../components/Layout.svelte';
+  import SEO from '../../../components/SEO.svelte';
+  import { vi } from '../../../utils/locale';
+  setContext('locale', vi)
+</script>
+
+<SEO />
+<Layout />
